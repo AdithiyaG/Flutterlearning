18a37a17febffe480aaa3302cd221bdd8c18ab90
AdithiyaG <adithiyagopal0912@gmail.com>
2021-04-15 19:09:59 +0530

    colors-n-fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,24 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text("App"),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
-        body: Center(child: Text("Hello")),
+        body: Center(
+          child: Text(
+            "This is flutter app",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'DancingScript',
+            ),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Text("click"),
+          backgroundColor: Colors.red[600],
         ),
       ),
     ));
